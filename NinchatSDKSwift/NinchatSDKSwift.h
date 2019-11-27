f1ad04173d1bd95526400a5d1ae8b8717011acea
Hassan Shahbazi <h-shahbazi@hotmail.com>
2019-11-27 17:28:52 +0200

    Initial Commit

---
+++ NinchatSDKSwift/NinchatSDKSwift.h
@@ -0,0 +1,19 @@
+//
+//  NinchatSDKSwift.h
+//  NinchatSDKSwift
+//
+//  Created by Hassan Shahbazi on 27.11.2019.
+//  Copyright © 2019 Hassan Shahbazi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NinchatSDKSwift.
+FOUNDATION_EXPORT double NinchatSDKSwiftVersionNumber;
+
+//! Project version string for NinchatSDKSwift.
+FOUNDATION_EXPORT const unsigned char NinchatSDKSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NinchatSDKSwift/PublicHeader.h>
+
+
