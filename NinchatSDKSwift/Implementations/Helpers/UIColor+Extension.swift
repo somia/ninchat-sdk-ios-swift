b2092d7bb51bb3d18de49e54dd8be9f38ce0dcd6
Hassan Shahbazi <h-shahbazi@hotmail.com>
2020-05-14 12:44:40 +0300

    add select element

--- NinchatSDKSwift/Implementations/Helpers/UIColor+Extension.swift
+++ NinchatSDKSwift/Implementations/Helpers/UIColor+Extension.swift
@@ -14,6 +14,16 @@ extension UIColor {
     static var grayButton: UIColor { #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1) }
 
     static var toastInfoBackground: UIColor { #colorLiteral(red: 0.0, green: 0.54117647, blue: 1.0, alpha: 1) }
+    
+    // MARK: - Questionnaire colors
+    
+    static var QGrayButton: UIColor { #colorLiteral(red: 0.6, green: 0.6, blue: 0.6, alpha: 1) }
+    
+    static var QRedBorder: UIColor { #colorLiteral(red: 1, green: 0, blue: 0.01176470588, alpha: 1) }
+    
+    static var QBlueButtonNormal: UIColor { #colorLiteral(red: 0, green: 0.5411764706, blue: 1, alpha: 1) }
+    
+    static var QBlueButtonHighlighted: UIColor { #colorLiteral(red: 0, green: 0.4352941176, blue: 0.8078431373, alpha: 1) }
 }
 
 extension UIColor {
