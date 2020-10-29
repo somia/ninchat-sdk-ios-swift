38534823d34d5fc2a28a21bf7cfcdb2dce3c24f7
Hassan Shahbazi <hassan@ninchat.com>
2020-10-29 15:40:07 +0200

    ChatView: resolve odd animation for attachment load

--- NinchatSDKSwift/Implementations/View/UIKit/Chat View/ChatView.swift
+++ NinchatSDKSwift/Implementations/View/UIKit/Chat View/ChatView.swift
@@ -188,14 +188,18 @@ extension ChatView {
             guard let weakSelf = self else { return }
             weakSelf.delegate?.didSelect(image: image, for: attachment, weakSelf)
         }
-        cell.onConstraintsUpdate = { [weak self] in
+        cell.onConstraintsUpdate = { [weak self, index = indexPath] in
+            guard self?.tableView.numberOfRows(inSection: 0) == self?.dataSource?.numberOfMessages(for: self!) else { return }
             cell.isReloading = true
-            UIView.animate(withDuration: TimeConstants.kAnimationDuration.rawValue, animations: {
-                guard self?.tableView.numberOfRows(inSection: 0) == self?.dataSource?.numberOfMessages(for: self!) else { return }
-                self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-            }, completion: { finished in
-                cell.isReloading = !finished
-            })
+
+            self?.tableView.beginUpdates()
+            DispatchQueue.global(qos: .userInitiated).async {
+                UIView.animate(withDuration: TimeConstants.kAnimationDuration.rawValue, animations: {
+                    DispatchQueue.main.async { /* self?.tableView.reloadRows(at: [index], with: .none) */ self?.tableView.endUpdates() }
+                }, completion: { finished in
+                    cell.isReloading = !finished
+                })
+            }
         }
         
         cell.populateChannel(message: message, configuration: self.sessionManager?.siteConfiguration, imageAssets: self.imageAssets, colorAssets: self.colorAssets, agentAvatarConfig: self.agentAvatarConfig, userAvatarConfig: self.userAvatarConfig, composeState: self.composeMessageStates?[message.messageID])
