d0c45d936a329123cc9df81757d5b18f029ac351
Hassan Shahbazi <hassan@ninchat.com>
2021-03-03 10:50:26 +0200

    Questionnaire: TypingCell: update to match new style

---
+++ NinchatSDKSwift/Implementations/View/UIKit/Questionnaire View/Cells/QuestionnaireTypingCell.swift
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 3.3.2021 Somia Reality Oy. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+import Foundation
+
+final class QuestionnaireTypingCell: ChatTypingCell {}
