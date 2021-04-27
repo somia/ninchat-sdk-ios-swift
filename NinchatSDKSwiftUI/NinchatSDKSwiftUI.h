aa4e5d585c500c749deacdafff09941158a7616f
Hassan Shahbazi <hassan@ninchat.com>
2021-04-27 13:41:29 +0300

    SwiftUI: add new scheme
    Signed-off-by: Hassan Shahbazi <hassan@ninchat.com>

---
+++ NinchatSDKSwiftUI/NinchatSDKSwiftUI.h
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 27.4.2021 Somia Reality Oy. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NinchatSDKSwiftUI.
+FOUNDATION_EXPORT double NinchatSDKSwiftUIVersionNumber;
+
+//! Project version string for NinchatSDKSwiftUI.
+FOUNDATION_EXPORT const unsigned char NinchatSDKSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NinchatSDKSwiftUI/PublicHeader.h>
+
+
