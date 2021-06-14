somia/ninchat-sdk-ios-swift

commit c9c903f1f2389c104d3e2b70c7a38c7601878610

Apply avatar rules
The commit applies required rules that must be followed for showing or
hiding avatars across the project, including the titlebar

NinchatSDKSwift/Implementations/Models/AvatarConfig.swift
@@ -7,19 +7,38 @@
 import Foundation
 
 struct AvatarConfig {
-    let show: Bool
     let imageOverrideURL: String?
     let nameOverride: String
+    var show: Bool!
 
     init(session: NINChatSessionManager?) {
         self.imageOverrideURL = session?.siteConfiguration.audienceQuestionnaireAvatar as? String
-        self.show = (session?.siteConfiguration.audienceQuestionnaireAvatar as? Bool) ?? true
         self.nameOverride = session?.siteConfiguration.audienceQuestionnaireUserName ?? ""
+        self.show = avatarVisibilityRules(session)
     }
+}
 
-    init(avatar: AnyHashable?, name: String?) {
-        self.imageOverrideURL = avatar as? String
-        self.show = (avatar as? Bool) ?? true
-        self.nameOverride = name ?? ""
+extension AvatarConfig {
+    /// Rules based on `https://github.com/somia/mobile/issues/343#issuecomment-857721302`
+    private func avatarVisibilityRules(_ session: NINChatSessionManager?) -> Bool {
+        guard let session = session else {
+            return false
+        }
+
+        /// 1- If titlebar is shown, agentAvatar is not shown ever in questionnaire or on backlog
+        if !session.siteConfiguration.hideTitlebar {
+            return false
+        }
+
+        /// 2- If agentAvatar: false, it's not shown anywhere
+        if let avatarBool = session.siteConfiguration.agentAvatar as? Bool, !avatarBool {
+            return false
+        }
+
+        /// 3- agentAvatar:true, show user_attrs.iconurl everywhere
+        /// 4- agentAvatar:url, show that instead
+        /// 5- no iconurl/agentAvatar, use avatar-male default icon
+        /// 6- no questionnaireAvatar, use gray placeholder ball and block
+        return true
     }
 }

NinchatSDKSwift/Implementations/View/UIKit/Chat View/ChatView.swift
@@ -95,8 +95,8 @@ final class ChatView: UIView, ChatViewProtocol {
             self.imageAssets = self.sessionManager?.delegate?.imageAssetsDictionary
             self.colorAssets = self.sessionManager?.delegate?.colorAssetsDictionary
 
-            self.agentAvatarConfig = AvatarConfig(avatar: sessionManager?.siteConfiguration.agentAvatar, name: sessionManager?.siteConfiguration.agentName)
-            self.userAvatarConfig = AvatarConfig(avatar: sessionManager?.siteConfiguration.userAvatar, name: sessionManager?.siteConfiguration.userName)
+            self.agentAvatarConfig = AvatarConfig(session: sessionManager)
+            self.userAvatarConfig = AvatarConfig(session: sessionManager)
         }
     }
     weak var dataSource: ChatViewDataSource?

NinchatSDKSwift/Implementations/View/UIKit/Confirm View/ConfirmVideoCallView.swift
@@ -82,7 +82,7 @@ final class ConfirmVideoCallView: UIView, ConfirmVideoCallViewProtocol, HasCusto
             self.usernameLabel.textColor = textColor
             self.infoLabel.textColor = textColor
         }
-        let agentAvatarConfig = AvatarConfig(avatar: sessionManager?.siteConfiguration.agentAvatar, name: sessionManager?.siteConfiguration.agentName)
+        let agentAvatarConfig = AvatarConfig(session: sessionManager)
         
         /// Caller's Avatar image
         if let overrideURL = agentAvatarConfig.imageOverrideURL {

NinchatSDKSwift/Implementations/View/Chat.storyboard
@@ -388,9 +388,8 @@ you are next.</string>
                                             <constraint firstItem="hb8-60-TGG" firstAttribute="centerY" secondItem="Yfi-1D-cO5" secondAttribute="centerY" id="Ivy-j4-Mv0"/>
                                             <constraint firstAttribute="trailing" secondItem="lpr-Ub-6OM" secondAttribute="trailing" constant="15" id="Jn8-kJ-NCV"/>
                                             <constraint firstItem="DbE-mm-dcs" firstAttribute="bottom" secondItem="lpr-Ub-6OM" secondAttribute="bottom" constant="-8" id="LJ9-jf-2FP"/>
-                                            <constraint firstItem="dEa-pc-H2B" firstAttribute="leading" secondItem="lpr-Ub-6OM" secondAttribute="leading" id="Umo-cR-RIt"/>
                                             <constraint firstItem="lpr-Ub-6OM" firstAttribute="top" secondItem="Zhu-12-0xh" secondAttribute="top" constant="55" id="ZrC-0j-UeC"/>
-                                            <constraint firstItem="lpr-Ub-6OM" firstAttribute="leading" secondItem="Yfi-1D-cO5" secondAttribute="leading" id="ar1-td-xxe"/>
+                                            <constraint firstItem="lpr-Ub-6OM" firstAttribute="leading" secondItem="Vt6-hF-T0g" secondAttribute="leading" id="b8P-Zp-jbh"/>
                                             <constraint firstItem="Vt6-hF-T0g" firstAttribute="leading" secondItem="Zhu-12-0xh" secondAttribute="leading" constant="15" id="cZc-p1-IPz"/>
                                             <constraint firstItem="dEa-pc-H2B" firstAttribute="top" secondItem="lpr-Ub-6OM" secondAttribute="bottom" constant="4" id="f26-LB-ZH6"/>
                                             <constraint firstItem="hb8-60-TGG" firstAttribute="leading" secondItem="Vt6-hF-T0g" secondAttribute="trailing" constant="8" id="fav-ef-l6O"/>
@@ -398,6 +397,7 @@ you are next.</string>
                                             <constraint firstItem="Vt6-hF-T0g" firstAttribute="top" secondItem="Zhu-12-0xh" secondAttribute="top" id="iDr-MJ-vkZ"/>
                                             <constraint firstItem="DbE-mm-dcs" firstAttribute="leading" secondItem="lpr-Ub-6OM" secondAttribute="leading" constant="8" id="iVh-g3-910"/>
                                             <constraint firstItem="dEa-pc-H2B" firstAttribute="trailing" secondItem="lpr-Ub-6OM" secondAttribute="trailing" id="pxn-7U-yar"/>
+                                            <constraint firstItem="dEa-pc-H2B" firstAttribute="leading" secondItem="Vt6-hF-T0g" secondAttribute="leading" id="rCY-gL-ufa"/>
                                             <constraint firstItem="DbE-mm-dcs" firstAttribute="top" secondItem="lpr-Ub-6OM" secondAttribute="top" constant="12" id="yGC-2b-b9P"/>
                                         </constraints>
                                     </view>
@@ -413,6 +413,11 @@ you are next.</string>
                                     <constraint firstItem="Zhu-12-0xh" firstAttribute="leading" secondItem="L5U-F0-l9Y" secondAttribute="leading" constant="16" id="g0K-By-o5x"/>
                                     <constraint firstAttribute="trailing" secondItem="Zhu-12-0xh" secondAttribute="trailing" constant="16" id="nho-Da-cI8"/>
                                 </constraints>
+                                <variation key="default">
+                                    <mask key="subviews">
+                                        <exclude reference="qj1-1U-Y4U"/>
+                                    </mask>
+                                </variation>
                             </view>
                             <button opaque="NO" contentMode="scaleToFill" contentHorizontalAlignment="center" contentVerticalAlignment="center" buttonType="system" lineBreakMode="middleTruncation" translatesAutoresizingMaskIntoConstraints="NO" id="PLV-Oi-4sR">
                                 <rect key="frame" x="182" y="800" width="50" height="30"/>
@@ -455,6 +460,7 @@ you are next.</string>
                         <outlet property="titlebar" destination="9Uk-ep-CyI" id="nDI-0G-wPZ"/>
                         <outlet property="topViewContainer" destination="L5U-F0-l9Y" id="L0c-wH-tU0"/>
                         <outlet property="userAvatar" destination="Yfi-1D-cO5" id="Btl-NB-cDx"/>
+                        <outlet property="userAvatarContainerView" destination="Vt6-hF-T0g" id="WbJ-hH-U0j"/>
                         <outlet property="userTitle" destination="vdr-sU-emu" id="6Rs-ny-yeZ"/>
                     </connections>
                 </viewController>

NinchatSDKSwift/Implementations/View/UIKit/Questionnaire View/Cells/QuestionnaireLoadingCell.swift
@@ -6,4 +6,4 @@
 
 import Foundation
 
-final class QuestionnaireTypingCell: ChatTypingCell {}
+final class QuestionnaireLoadingCell: ChatTypingCell {}

NinchatSDKSwift/Implementations/View/UIKit/Questionnaire View/Cells/QuestionnaireLoadingCell.xib
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="17701" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="18122" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES">
     <device id="retina6_1" orientation="portrait" appearance="light"/>
     <dependencies>
         <deployment identifier="iOS"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="17703"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="18093"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
     </dependencies>
     <customFonts key="customFonts">
@@ -14,7 +14,7 @@
     <objects>
         <placeholder placeholderIdentifier="IBFilesOwner" id="-1" userLabel="File's Owner"/>
         <placeholder placeholderIdentifier="IBFirstResponder" id="-2" customClass="UIResponder"/>
-        <tableViewCell clipsSubviews="YES" contentMode="scaleToFill" preservesSuperviewLayoutMargins="YES" selectionStyle="default" indentationWidth="10" rowHeight="119" id="zrG-yv-IFz" userLabel="ChatBubbleCell" customClass="QuestionnaireTypingCell" customModule="NinchatSDKSwift">
+        <tableViewCell clipsSubviews="YES" contentMode="scaleToFill" preservesSuperviewLayoutMargins="YES" selectionStyle="default" indentationWidth="10" rowHeight="119" id="zrG-yv-IFz" userLabel="ChatBubbleCell" customClass="QuestionnaireLoadingCell" customModule="NinchatSDKSwift">
             <rect key="frame" x="0.0" y="0.0" width="356" height="119"/>
             <autoresizingMask key="autoresizingMask"/>
             <tableViewCellContentView key="contentView" opaque="NO" clipsSubviews="YES" multipleTouchEnabled="YES" tag="666" contentMode="center" preservesSuperviewLayoutMargins="YES" insetsLayoutMarginsFromSafeArea="NO" tableViewCell="zrG-yv-IFz" id="i7n-Bp-vop">

NinchatSDKSwift/Implementations/View/ViewController/NINChatViewController.swift
@@ -176,7 +176,9 @@ final class NINChatViewController: UIViewController, ViewController, KeyboardHan
         }
     }
     var titlebarAvatar: String? {
-        self.sessionManager?.agent?.iconURL
+        /// - agentAvatar:true, show user_attrs.iconurl everywhere
+        /// - agentAvatar:url, show that instead
+        (self.sessionManager?.siteConfiguration.agentAvatar as? String) ?? (self.sessionManager?.agent?.iconURL)
     }
     var titlebarName: String? {
         self.sessionManager?.agent?.displayName

NinchatSDKSwift/Implementations/View/ViewController/NINQuestionnaireViewController.swift
@@ -16,7 +16,7 @@ protocol QuestionnaireConversationController {
     func updateConversationContentView(_ interval: TimeInterval)
 }
 
-final class NINQuestionnaireViewController: UIViewController, ViewController, KeyboardHandler, HasTitleBar, HasDefaultAvatar {
+final class NINQuestionnaireViewController: UIViewController, ViewController, KeyboardHandler, HasTitleBar {
 
     private let operationQueue = OperationQueue.main
     private let dispatchQueue = DispatchQueue.main
@@ -157,10 +157,6 @@ final class NINQuestionnaireViewController: UIViewController, ViewController, Ke
         self.sessionManager?.siteConfiguration.audienceQuestionnaireUserName
     }
     private(set) var titlebarJob: String? = nil
-
-    // MARK: - HasTitleBar
-
-    private(set) var defaultAvatar: UIImage? = UIImage(named: "icon_avatar_other", in: .SDKBundle, compatibleWith: nil)
 
     // MARK: - UIViewController life-cycle
 

commit f53ef98180c483474e8a0874f7a355e71a1250d0

Avatar: fix issue in initialization
The commit fixes an issue caused by commit c9c903f1f2 that resulted in using
questionnaire info (avatar,name) everywhere.

NinchatSDKSwift/Implementations/Data Source - Delegate/Questionnaire/NINQuestionnaireConversationDataSourceDelegate.swift
@@ -138,7 +138,7 @@ extension NINQuestionnaireConversationDataSourceDelegate: QuestionnaireConversat
 extension NINQuestionnaireConversationDataSourceDelegate {
     private func loading(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: QuestionnaireLoadingCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
-        cell.populateLoading(agentAvatarConfig: AvatarConfig(session: self.sessionManager),
+        cell.populateLoading(agentAvatarConfig: AvatarConfig(forQuestionnaire: self.sessionManager),
                 imageAssets: self.delegate?.imageAssetsDictionary ?? [:],
                 colorAssets: self.delegate?.colorAssetsDictionary ?? [:])
 

NinchatSDKSwift/Implementations/Models/AvatarConfig.swift
@@ -11,10 +11,23 @@ struct AvatarConfig {
     let nameOverride: String
     var show: Bool!
 
-    init(session: NINChatSessionManager?) {
+    init(forQuestionnaire session: NINChatSessionManager?) {
         self.imageOverrideURL = session?.siteConfiguration.audienceQuestionnaireAvatar as? String
         self.nameOverride = session?.siteConfiguration.audienceQuestionnaireUserName ?? ""
         self.show = avatarVisibilityRules(session)
+    }
+
+    init(forAgent session: NINChatSessionManager?) {
+        self.imageOverrideURL = session?.siteConfiguration.agentAvatar as? String
+        self.nameOverride = session?.siteConfiguration.agentName ?? ""
+        self.show = avatarVisibilityRules(session)
+    }
+
+    init(forUser session: NINChatSessionManager?) {
+        self.imageOverrideURL = session?.siteConfiguration.userAvatar as? String
+        self.nameOverride = session?.siteConfiguration.userName ?? ""
+        /// avatar visibility rules are not applied to user avatar
+        self.show = (session?.siteConfiguration.userAvatar as? Bool) ?? true
     }
 }
 

NinchatSDKSwift/Implementations/View/UIKit/Chat View/ChatView.swift
@@ -95,8 +95,8 @@ final class ChatView: UIView, ChatViewProtocol {
             self.imageAssets = self.sessionManager?.delegate?.imageAssetsDictionary
             self.colorAssets = self.sessionManager?.delegate?.colorAssetsDictionary
 
-            self.agentAvatarConfig = AvatarConfig(session: sessionManager)
-            self.userAvatarConfig = AvatarConfig(session: sessionManager)
+            self.agentAvatarConfig = AvatarConfig(forAgent: sessionManager)
+            self.userAvatarConfig = AvatarConfig(forUser: sessionManager)
         }
     }
     weak var dataSource: ChatViewDataSource?

NinchatSDKSwift/Implementations/View/UIKit/Confirm View/ConfirmVideoCallView.swift
@@ -82,7 +82,7 @@ final class ConfirmVideoCallView: UIView, ConfirmVideoCallViewProtocol, HasCusto
             self.usernameLabel.textColor = textColor
             self.infoLabel.textColor = textColor
         }
-        let agentAvatarConfig = AvatarConfig(session: sessionManager)
+        let agentAvatarConfig = AvatarConfig(forAgent: sessionManager)
         
         /// Caller's Avatar image
         if let overrideURL = agentAvatarConfig.imageOverrideURL {

NinchatSDKSwift/Implementations/View/UIKit/Questionnaire View/Cells/QuestionnaireCell.swift
@@ -56,7 +56,7 @@ class QuestionnaireCell: UITableViewCell {
     }
 
     private func setupAvatar(_ userAvatar: UIImageView) {
-        let avatar = AvatarConfig(session: self.sessionManager)
+        let avatar = AvatarConfig(forQuestionnaire: self.sessionManager)
 
         userAvatar.isHidden = !avatar.show
         leftAvatarContainerView.width?.constant = (userAvatar.isHidden) ? 0 : 35