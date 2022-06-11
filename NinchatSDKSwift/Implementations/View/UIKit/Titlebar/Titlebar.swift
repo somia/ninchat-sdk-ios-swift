649c419eb51d88005a8ee2fb35d5717210b5288d
Hassan Shahbazi <hassan@shahbazi.fi>
2022-06-11 20:18:06 +0300

    Titlebar: refactor and update implementation with the new parameter

--- NinchatSDKSwift/Implementations/View/UIKit/Titlebar/Titlebar.swift
+++ NinchatSDKSwift/Implementations/View/UIKit/Titlebar/Titlebar.swift
@@ -59,45 +59,17 @@ final class Titlebar: UIView {
     
     // MARK: - Setup View
     
-    func setupView(_ session: NINChatSessionManager?, view: HasTitleBar, defaultAvatarView: HasDefaultAvatar?) {
-        if let name = view.titlebarName, !name.isEmpty {
-            /// show titlebar
-            agentInfoPlaceholder.isHidden = true
-            
-            agentNameLabel.text = name
-        } else {
-            /// show placeholder
-            agentInfoStackView.isHidden = true
+    func setupView(_ session: NINChatSessionManager?, _ showAvatar: Bool? = nil, view: HasTitleBar, defaultAvatarView: HasDefaultAvatar?) {
+        self.setupName(view)
+        self.setupJob(view)
+        
+        self.agentAvatarContainer.isHidden = self.setupAvatarContainer(session, showAvatar: showAvatar)
+        if !agentAvatarContainer.isHidden {
+            self.setupAvatar(view, defaultAvatar: defaultAvatarView?.defaultAvatar)
         }
         
-        if let job = view.titlebarJob, !job.isEmpty {
-            agentJobLabel.text = job
-        } else {
-            /// Hide job if not available
-            agentJobLabel.isHidden = true
-        }
-        
-        if agentInfoStackView.isHidden {
-            /// show placeholder if name is hidden
-            agentAvatarImageView.isHidden = true
-        } else if let agentAvatar = session?.siteConfiguration.agentAvatar as? Bool, !agentAvatar {
-            /// don't show avatar if config.agentAvatar = false
-            agentAvatarContainer.isHidden = true
-        } else if let avatarImage = view.titlebarAvatar, !avatarImage.isEmpty {
-            agentAvatarContainer.backgroundColor = .white
-            agentAvatarImageView.image(from: avatarImage, defaultImage: defaultAvatarView?.defaultAvatar ?? UIImage())
-        } else if let defaultAvatar = defaultAvatarView?.defaultAvatar {
-            agentAvatarContainer.backgroundColor = .white
-            agentAvatarImageView.image = defaultAvatar
-        } else {
-            agentAvatarContainer.isHidden = true
-        }
-
-        closeButton.buttonTitle = session?.translate(key: Constants.kCloseText.rawValue, formatParams: [:]) ?? ""
+        self.setupCloseButton(session)
         overrideAssets(delegate: session?.delegate)
-        
-        guard let session = session else { return }
-        closeButton.isHidden = session.siteConfiguration.hideTitlebar
     }
     
     private func overrideAssets(delegate: NINChatSessionInternalDelegate?) {
@@ -107,5 +79,60 @@ final class Titlebar: UIView {
             agentInfoPlaceholder.backgroundColor = placeholder
             if agentAvatarImageView.isHidden { agentAvatarContainer.backgroundColor = placeholder }
         }
+    }
+}
+
+private extension Titlebar {
+    private func setupName(_ view: HasTitleBar) {
+        if let name = view.titlebarName, !name.isEmpty {
+            /// show titlebar
+            agentInfoPlaceholder.isHidden = true
+            agentNameLabel.text = name
+        } else {
+            /// show placeholder
+            agentInfoStackView.isHidden = true
+        }
+    }
+    
+    private func setupJob(_ view: HasTitleBar) {
+        if let job = view.titlebarJob, !job.isEmpty {
+            agentJobLabel.text = job
+        } else {
+            /// Hide job if not available
+            agentJobLabel.isHidden = true
+        }
+    }
+    
+    private func setupAvatarContainer(_ session: NINChatSessionManager?, showAvatar: Bool?) -> Bool {
+        /// Show/Hide avatar
+        if agentInfoStackView.isHidden {
+            /// show placeholder if name is hidden
+            return true
+        } else if let showAvatar = showAvatar {
+            /// set the avatar status explicitly
+            return !showAvatar
+        } else if let agentAvatar = session?.siteConfiguration.agentAvatar as? Bool, !agentAvatar {
+            /// don't show avatar if config.agentAvatar = false
+            return true
+        }
+        return true
+    }
+    
+    private func setupAvatar(_ view: HasTitleBar, defaultAvatar: UIImage?) {
+        /// Set Avatar
+        if let avatarImage = view.titlebarAvatar, !avatarImage.isEmpty {
+            agentAvatarContainer.backgroundColor = .white
+            agentAvatarImageView.image(from: avatarImage, defaultImage: defaultAvatar ?? UIImage())
+        } else if let defaultAvatar = defaultAvatar {
+            agentAvatarContainer.backgroundColor = .white
+            agentAvatarImageView.image = defaultAvatar
+        }
+    }
+    
+    private func setupCloseButton(_ session: NINChatSessionManager?) {
+        closeButton.buttonTitle = session?.translate(key: Constants.kCloseText.rawValue, formatParams: [:]) ?? ""
+        
+        guard let session = session else { return }
+        closeButton.isHidden = session.siteConfiguration.hideTitlebar
     }
 }
