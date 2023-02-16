4eeea86176a7a4fe8ec857dd2705974060dba413
Alex Voronov <voronovaleksandr91@gmail.com>
2023-02-16 12:41:47 +0200

    fixing crashed caused by alive subscription

--- NinchatSDKSwift/Implementations/View/ViewController/NINChatViewController.swift
+++ NinchatSDKSwift/Implementations/View/ViewController/NINChatViewController.swift
@@ -255,7 +255,7 @@ final class NINChatViewController: UIViewController, ViewController, KeyboardHan
         self.deallocViewModel()
         self.removeKeyboardListeners()
 
-        NotificationCenter.default.removeObserver(self, name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
         NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
     }
 
