409e60d34061bc17648916bffa025a95629af137
pallab-gain-tt <pallab.gain@terveystalo.com>
2023-12-27 16:07:18 +0200

    bugfix/crash-on-av-permission-modal: remove spacing due to IDE setting

--- NinchatSDKSwift/Implementations/View/UIKit/Video View/JitsiVideoWebView.swift
+++ NinchatSDKSwift/Implementations/View/UIKit/Video View/JitsiVideoWebView.swift
@@ -94,15 +94,6 @@ extension JitsiVideoWebView {
             webView.load(urlRequest)
         }
     }
-
-    @available(iOS 15.0, *)
-    func webView(_ webView: WKWebView,
-                 requestMediaCapturePermissionFor origin: WKSecurityOrigin,
-                 initiatedBy frame: WKFrameInfo,
-                 type: WKMediaCaptureType,
-                 decisionHandler: @escaping (WKPermissionDecision) -> Void) {
-        decisionHandler(.grant)
-    }
 }
 
 // MARK: - Finishing a Jitsi meeting
@@ -123,7 +114,7 @@ extension JitsiVideoWebView: WKUIDelegate {
     @available(iOS 15.0, *)
     func webView(_ webView: WKWebView, requestMediaCapturePermissionFor origin: WKSecurityOrigin, initiatedByFrame frame: WKFrameInfo, type: WKMediaCaptureType, decisionHandler: @escaping (WKPermissionDecision) -> Void) {
         decisionHandler(.grant)
-    }   
+    }
 }
 
 // MARK: - WKScriptMessageHandler
