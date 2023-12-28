5442f1fd4f27388341c2719fa28fe3c40b34c77c
Pallab Gain <122030825+pallab-gain-tt@users.noreply.github.com>
2023-12-28 12:19:20 +0200

    SDK v0.8.2 (#409)

--- NinchatSDKSwift/Implementations/View/UIKit/Video View/JitsiVideoWebView.swift
+++ NinchatSDKSwift/Implementations/View/UIKit/Video View/JitsiVideoWebView.swift
@@ -15,23 +15,23 @@ protocol JitsiVideoWebViewEventsDelegate: AnyObject {
 class JitsiVideoWebView: UIView {
 
     // MARK: - PROPERTIES
-    
+
     // MARK: - Web view
     private var webView: WKWebView!
-    
+
     // MARK: - Delegate
     var eventsDelegate: JitsiVideoWebViewEventsDelegate?
-    
+
     // MARK: - Debug mode
     private var isDebugMode = false
-    
+
     // MARK: - INITIALIZERS
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .clear
         self.addWebView()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.backgroundColor = .clear
@@ -50,27 +50,28 @@ extension JitsiVideoWebView {
             webConfig.defaultWebpagePreferences.preferredContentMode = .mobile // this setting is necessary for iPad
         }
         // webConfig.preferences.setValue(true, forKey: "developerExtrasEnabled") // uncomment this if you want to debug using the web inspector
-        
+
         // Enable JavaScript messaging
         let contentController = WKUserContentController()
         contentController.add(self, name: "videoConferenceJoined")
         contentController.add(self, name: "videoConferenceLeft")
         webConfig.userContentController = contentController
-        
+
         self.webView = WKWebView(frame: .zero, configuration: webConfig)
-        
+
         // Use black background for the web view while it loads Jitsy video
         webView.backgroundColor = .black
         webView.isOpaque = false
         webView.scrollView.backgroundColor = .black
         webView.scrollView.isScrollEnabled = false
-        
+        webView.uiDelegate = self
+
         // Set a custom non-iPhone user agent, otherwise Jitsi might not load
         webView.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-        
+
         // Set navigation delegate
         webView.navigationDelegate = self
-        
+
         // Add web view
         webView.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(webView)
@@ -109,6 +110,13 @@ extension JitsiVideoWebView: WKNavigationDelegate {
     }
 }
 
+extension JitsiVideoWebView: WKUIDelegate {
+    @available(iOS 15.0, *)
+    func webView(_ webView: WKWebView, requestMediaCapturePermissionFor origin: WKSecurityOrigin, initiatedByFrame frame: WKFrameInfo, type: WKMediaCaptureType, decisionHandler: @escaping (WKPermissionDecision) -> Void) {
+        decisionHandler(.grant)
+    }
+}
+
 // MARK: - WKScriptMessageHandler
 extension JitsiVideoWebView: WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
@@ -130,7 +138,7 @@ extension JitsiVideoWebView {
         let roomName = components.queryItems?.first(where: { $0.name == "roomName" })?.value ?? ""
         let jwt = components.queryItems?.first(where: { $0.name == "jwt" })?.value ?? ""
         let lang = components.queryItems?.first(where: { $0.name == "lang" })?.value ?? "en"
-        
+
         let htmlContent = """
         <!DOCTYPE html>
         <html>
@@ -187,7 +195,7 @@ extension JitsiVideoWebView {
             </body>
         </html>
         """
-        
+
         return htmlContent
     }
 }
